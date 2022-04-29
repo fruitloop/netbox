4ab6c797bef0f53033e40d6696b3f764747150d0
jeremystretch <jstretch@ns1.com>
2022-04-29 09:16:49 -0400

    Revert initial model changes

--- netbox/dcim/filtersets.py
+++ netbox/dcim/filtersets.py
@@ -1473,9 +1473,9 @@ class VirtualChassisFilterSet(NetBoxModelFilterSet):
 
 class CableFilterSet(TenancyFilterSet, NetBoxModelFilterSet):
     termination_a_type = ContentTypeFilter()
-    termination_a_ids = MultiValueNumberFilter()
+    termination_a_id = MultiValueNumberFilter()
     termination_b_type = ContentTypeFilter()
-    termination_b_ids = MultiValueNumberFilter()
+    termination_b_id = MultiValueNumberFilter()
     type = django_filters.MultipleChoiceFilter(
         choices=CableTypeChoices
     )
@@ -1511,7 +1511,7 @@ class CableFilterSet(TenancyFilterSet, NetBoxModelFilterSet):
 
     class Meta:
         model = Cable
-        fields = ['id', 'label', 'length', 'length_unit', 'termination_a_ids', 'termination_b_ids']
+        fields = ['id', 'label', 'length', 'length_unit', 'termination_a_id', 'termination_b_id']
 
     def search(self, queryset, name, value):
         if not value.strip():
