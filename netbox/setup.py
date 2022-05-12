8cdf8450caf30fc4e1bb0992fc0b1d6fd36591ac
Roland Planitz <github@planitz.at>
2022-05-12 09:43:49 +0200

    Update setup.py

--- netbox/setup.py
+++ netbox/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, '../README.md')) as f: readme = f.read()
 
 setup(
     name = 'netbox-pip',
-    version = '0.0.1',
+    version = '0.0.2',
 
     description = 'netbox but on pypi',
     long_description = readme,
