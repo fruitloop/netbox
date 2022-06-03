1f8279a90e2b6ac92691accc4b2893d7a38d0794
Roland Planitz <roland@planitz.at>
2022-06-03 12:35:50 +0200

    debugging and fixing stupid typos

--- netbox/extras/ssl_auth.py
+++ netbox/extras/ssl_auth.py
@@ -5,13 +5,19 @@ Use SSL Client Certs for authentication
 """
 
 def _dictify_dn(dn):
+    # XXX
+    with open('/tmp/dictify_dn','a') as f: f.write('line:%s\n\n' % dn)
+
     return dict(x.split('=') for x in dn.split('/') if '=' in x)
 
 def user_dict_from_cert(dn):
-    print(dn)
+    # XXX
+    with open('/tmp/user_dict_from_cert','a') as f: f.write('line:%s\n\n' % dn)
+
+    d = _dictify_dn(dn)
     out = dict()
     out['username'] = d['CN']
-    out['first_name'] = 'Arno'
-    out['last_name'] = 'Nym'
+    out['first_name'] = ''
+    out['last_name'] = ''
     out['email'] = ''
     return out
